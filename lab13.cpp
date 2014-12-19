d97dadef3ff9346973ff0d0f0f86f84afb1e9932
eayan <erdal_ayan@yahoo.com>
2014-12-19 14:20:48 +0200

    lab13.cpp

---
+++ lab13.cpp
@@ -0,0 +1,45 @@
+//first one
+#include <iostream.h>
+void main()
+{
+	cout <<"Hello, World!" << endl;
+	return 0;
+}
+
+//second one
+#include <iostream>
+void main()
+{
+	cout << "Hello, World!" << endl;
+}
+
+//third one
+#include <iostream>
+void main()
+{
+	cout << "Hello, World!" << endl;
+}
+
+//fourth one
+#include <iostream>
+void main()
+{
+	cout << "Hello, World!" << endl;
+}
+
+#include <iostream>
+void main()
+{
+	float num1;
+	float num2;
+	char op1;
+	float ans;
+	cout << "Please enter a number: ";
+	cin >> num1;
+	cout << "Please enter another number: ";
+	cin >> num2;
+	cout << "Press A to add the two numbers. ";
+
+
+}
+
